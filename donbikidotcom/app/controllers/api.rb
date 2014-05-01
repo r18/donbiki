4e5fe5d2b05fb7f5f9cd73b1b179af86080153a6
hrl7 <hrlclb@gmail.com>
2014-05-01 15:59:18 +0900

    Fix

--- donbikidotcom/app/controllers/api.rb
+++ donbikidotcom/app/controllers/api.rb
@@ -1,6 +1,6 @@
 Donbikidotcom::App.controllers :api do
 
-  post "/post_dtweet" do
+  post "/post_dtweet", :csrf_protection => false do
     @dtweet = Dtweet.new(params.require(:dtweet).permit(:user, :body, :text, :tweetId))
     if @dtweet.save
       puts "Success"
